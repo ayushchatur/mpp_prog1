1d7a2d26bb434dc27a4b5e465482dc85d37f87e3
ayush <ayush.6200@gmail.com>
2021-10-03 19:10:10 -0400

    completed

--- src/mutex/BakeryTest.java
+++ src/mutex/BakeryTest.java
@@ -7,35 +7,30 @@
 
 package mutex;
 
-import junit.framework.*;
+import mutex.Bakery;
+import mutex.ThreadID;
 
 /**
  * Crude & inadequate test of lock class.
  * @author Maurice Herlihy
  */
-public class BakeryTest extends TestCase {
+public class BakeryTest {
   private final static int THREADS = 16;
   private final static int COUNT = 1024;
   private final static int PER_THREAD = COUNT / THREADS;
-  Thread[] thread = new Thread[THREADS];
+  Thread[] thread = new MyThread[THREADS];
   int counter = 0;
   
-  Bakery instance = new Bakery(THREADS);
-  
-  public BakeryTest(String testName) {
-    super(testName);
-  }
-  
-  public static Test suite() {
-    TestSuite suite = new TestSuite(BakeryTest.class);
+ Bakery instance = new Bakery(THREADS);
     
-    return suite;
-  }
+    
   
-  public void testParallel() throws Exception {
+  public boolean testParallel() throws Exception {
     ThreadID.reset();
     for (int i = 0; i < THREADS; i++) {
       thread[i] = new MyThread();
+      StringBuilder s = new StringBuilder().append(i);
+      thread[i].setName(s.toString());
     }
     for (int i = 0; i < THREADS; i++) {
       thread[i].start();
@@ -44,7 +39,10 @@ public class BakeryTest extends TestCase {
       thread[i].join();
     }
       System.out.println(counter);
-    assertEquals(counter, COUNT);
+//    assertEquals(counter, COUNT);
+      
+    return (counter == COUNT);
+      
   }
   
   class MyThread extends Thread {
@@ -60,7 +58,7 @@ public class BakeryTest extends TestCase {
           instance.unlock();
         }
         long timeF = System.nanoTime();
-          System.out.println("time take by thread id: "  + "is: " + (timeF - timS));
+          System.out.println("time take by thread id: " + this.getName() + " is: " + (timeF - timS));
       }
     }
   }
---
+++ test/mutex/NewMain.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mutex;
+
+/**
+ *
+ * @author ayush
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+           
+         BakeryTest i = new BakeryTest();
+         try
+         {
+             
+         boolean result = i.testParallel();
+             System.err.println("result is " + result);
+         }
+         catch(Exception ex)
+         {
+             
+         }
+    }
+    
+}
