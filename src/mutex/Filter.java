0b699c25624ad55c88d9cbad924c20b1c267f8e5
ayush <ayush.6200@gmail.com>
2021-10-04 21:54:31 -0400

    asdasdf

--- src/mutex/Filter.java
+++ src/mutex/Filter.java
@@ -24,35 +24,32 @@ import java.util.concurrent.locks.Lock;
 class Filter implements Lock {
   
     int N;
-   volatile AtomicInteger[] flags;
-    volatile AtomicInteger[] victim;
+    volatile AtomicInteger[] levels;
+     volatile AtomicInteger[] victim;
     
     public Filter(int threads ) {
       this.N = threads;
-      this.flags = new AtomicInteger[N];
+      this.levels = new AtomicInteger[N];
       this.victim = new AtomicInteger[N];
         for (int i = 0; i < N; i++) {
             this.victim[i] = new AtomicInteger();
-            this.flags[i] = new AtomicInteger();
+            this.levels[i] = new AtomicInteger();
         }
       
   }
   public void lock() {
    int id = ThreadID.get();
-      for (int i = 1  ; i < N; i++) {
-          
-          this.flags[id].set(i);
-          this.victim[i].set(id);
+  for (int i = 1; i < N; i++) {
+        levels[id].set(i);
+        victim[i].set(id);
 
-          for( int p=0;p<N;p++)
-          {
-            while( (p !=id ) && (victim[p].get() >= i && victim[i].get() == id))
-            {
-              //spin wait
-            } 
-          }
-          
-      }
+            for (int k = 0; k < N; k++) {
+                while ( (k != id) && ((levels[k].get() >= i) && (victim[i].get() == id))) {                
+                  // spin wait
+                }
+            
+        }
+    }
    
   }
   // Is there another thread at the same or higher level?
@@ -60,7 +57,7 @@ class Filter implements Lock {
   public void unlock() {
       int id = ThreadID.get();
       
-      this.victim[id].set (0);
+      this.levels[id].set(0);
 
   }
   
